1535df6d7b9893bdfc78139cafce10fe29f406b3
praveen-16-16 <rajp18733@gmail.com>
2025-03-23 17:17:19 +0530

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,8 +1,16 @@
 from arithmetics import add
 
-def test_add():
+def addd():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
+
+    result = add(a,b)
+
+    print(f"\nResult of Addition ({a}, {b}) = {result}")
+
+def test_add():
+    a = 5
+    b = 10
 
     result = add(a,b)
 
